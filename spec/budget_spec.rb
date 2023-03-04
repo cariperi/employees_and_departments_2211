e754a4a1f28da1584b5d4c0599364ccccff45e90
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-04 18:57:59 -0500

    test: add initialize test for budget class

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Budget do
+  before(:each) do
+    @budget = Budget.new('2023')
+    @customer_service = Department.new("Customer Service")
+    @parks = Department.new('Parks')
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    @aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+  end
 
+  describe 'initialize' do
+    it 'exists' do
+      expect(@budget).to be_a Budget
+    end
+
+    it 'has a year' do
+      expect(@budget.year).to eq('2023')
+    end
+
+    it 'has departments that are empty by default' do
+      expect(@budget.departments).to eq([])
+    end
+  end
 end
