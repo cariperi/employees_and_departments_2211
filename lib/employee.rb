7120b598d28a236e96bded2da4113bf7a4d075ea
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-04 18:20:13 -0500

    feat: add initialize method and attributes

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,11 @@
 class Employee
+  attr_reader :name,
+              :age,
+              :salary
 
+  def initialize(details)
+    @name = details[:name]
+    @age = details[:age].to_i
+    @salary = details[:salary].delete("$").to_i
+  end
 end
