b6a11cf2efacd0b014cdafc55472356bde23676b
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-04 18:18:50 -0500

    test: add initialize method tests

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Employee do
+  before(:each) do
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+  end
 
+  describe 'initialize' do
+    it 'exists' do
+      expect(@bobbi).to be_a Employee
+    end
+
+    it 'has attributes' do
+      expect(@bobbi.name).to eq("Bobbi Jaeger")
+      expect(@bobbi.age).to eq(30)
+      expect(@bobbi.salary).to eq(100000)
+    end
+  end
 end
