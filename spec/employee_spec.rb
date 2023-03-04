feb4afbe763ec65ac8e14455623e85a048196a2b
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-04 18:21:36 -0500

    test: add test for give_raise method

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -5,7 +5,7 @@ describe Employee do
     @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
   end
 
-  describe 'initialize' do
+  describe '#initialize' do
     it 'exists' do
       expect(@bobbi).to be_a Employee
     end
@@ -14,6 +14,16 @@ describe Employee do
       expect(@bobbi.name).to eq("Bobbi Jaeger")
       expect(@bobbi.age).to eq(30)
       expect(@bobbi.salary).to eq(100000)
+    end
+  end
+
+  describe '#give_raise' do
+    it 'can add dollars to an employees salary' do
+      expect(@bobbi.salary).to eq(100000)
+
+      @bobbi.give_raise(5000)
+
+      expect(@bobbi.salary).to eq(105000)
     end
   end
 end
