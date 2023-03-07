5a4915c6af87caab2d2fd17dda105fae87a49716
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-06 22:24:18 -0500

    feat: update expense method to track expenses by employee as attribute

--- lib/department.rb
+++ lib/department.rb
@@ -1,19 +1,22 @@
 class Department
   attr_reader :name,
               :employees,
-              :expenses
+              :expenses,
+              :expenses_by_employee
 
   def initialize(name)
     @name = name
     @employees = []
     @expenses = 0
+    @expenses_by_employee = Hash.new(0)
   end
 
   def hire(employee)
     @employees << employee
   end
 
-  def expense(amount)
+  def expense(amount, employee)
+    @expenses_by_employee[employee] += amount
     @expenses += amount
   end
 end
